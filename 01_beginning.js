ae8cb3ee3cc45a1ec5599c12208fbed6f0c4abb7
BossOfGym <pamat2002@mail.ru>
2021-03-06 12:01:00 +0300

    Update 01_beginning.js

--- 01_beginning.js
+++ 01_beginning.js
@@ -1,12 +1,12 @@
-let stop = 6;
+﻿let stop = 6;
 let index;
 for (index = 1; index <= stop; index++){
     switch(index){
         case 5: console.log("пять "); break;
-        case 13: console.log("тринадцать"); break;
-        case 22: console.log("двадцать два"); break;
-        case 35: console.log("тридцать пять"); break;
-        case 98: console.log("девяносто восемь"); break;
+        case 13: console.log("тринадцать "); break;
+        case 22: console.log("двадцать два "); break;
+        case 35: console.log("тридцать пять "); break;
+        case 98: console.log("девяносто восемь "); break;
         default: console.log(index + " ")
     }
 }
